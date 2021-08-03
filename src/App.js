f931b9e40bb1b97a39842a62ee4a461873847aaa
Levi Liester <levi.liester@gmail.com>
2021-08-03 05:08:37 -0500

    eslint, react-stripe
    Stripe not hooked up.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import Mondrian from 'mondrian-art';
+import React, { useEffect } from 'react'
+import './App.css'
+import Mondrian from 'mondrian-art'
 import SaveSvgAsPng from 'save-svg-as-png'
-import {useEffect} from "react";
+
+import { loadStripe } from '@stripe/stripe-js'
+import {
+  Elements
+} from '@stripe/react-stripe-js'
+import CheckoutForm from './checkout-form/CheckoutForm'
 
 const mondrianContainerId = 'my-mondrian'
 
-function App() {
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh')
 
+function App () {
+  let mondrian
 
-    let mondrian;
+  const downloadMondrian = () => {
+    SaveSvgAsPng.saveSvgAsPng(document.getElementById(mondrianContainerId).children[0], 'my-mondrian.png')
+  }
 
-    const downloadMondrian = () => {
-        SaveSvgAsPng.saveSvgAsPng(document.getElementById(mondrianContainerId).children[0], 'my-mondrian.png')
-    }
+  useEffect(() => {
+    mondrian = new Mondrian({
+      container: document.getElementById(mondrianContainerId),
+      width: 700,
+      height: 700,
+      mondrian: {
+        style: 'classic'
+      }
+    })
 
-    useEffect(() => {
-        mondrian = new Mondrian({
-            container: document.getElementById(mondrianContainerId),
-            width: 700,
-            height: 700,
-            mondrian: {
-                style: 'classic',
-            }
-        });
+    mondrian.generate()
+  }, [])
 
-        mondrian.generate();
-    },[])
-
-    return (
-        <div className="App">
-            <div className={"mondrianContainer"}>
-                <div className={'title'}>My Mondrian</div>
-                <div id={"my-mondrian"}/>
-                <div className={'controls'}>
-                    <button onClick={downloadMondrian}>Download</button>
-                    <button onClick={() => mondrian.generate()}>Generate</button>
-                </div>
-            </div>
-            <div className={'stripeContainer'}>
-                Stripe Goes Here
+  return (
+    <div className="App">
+        <div className={'mondrianContainer'}>
+            <div className={'title'}>My Mondrian</div>
+            <div id={'my-mondrian'}/>
+            <div className={'controls'}>
+                <button onClick={downloadMondrian}>Download</button>
+                <button style={{ marginLeft: '1em' }} onClick={() => mondrian.generate()}>Generate</button>
             </div>
         </div>
-    );
+        <div className={'stripeContainer'}>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
+        </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
